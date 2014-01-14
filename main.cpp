897b506658d95166c7f961dd7975774ce0335567
Miguel Freitas <miguelfreitas@users.noreply.github.com>
2014-01-14 19:37:00 -0200

    add default seeders

--- main.cpp
+++ main.cpp
@@ -343,7 +343,7 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {""};
+static const string mainnet_seeds[] = {"seed.twister.net.co","seed2.twister.net.co","seed3.twister.net.co"};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
